d6be4b6c2bc6fc9847b4b1d1ba2d9d3c488a0472
morningshane <70739280+morningshane@users.noreply.github.com>
2020-09-20 19:00:56 +1000

    Update infowindow.py

--- mod_infowindow/infowindow.py
+++ mod_infowindow/infowindow.py
@@ -2,7 +2,7 @@ from driver import epd7in5b
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import epdconfig
+from driver import epdconfig
 import os, sys
 
 class InfoWindow():
